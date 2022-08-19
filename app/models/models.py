f6b3e4d0fe7395b6b819767141d10fbd19ca4403
twatkins92 <thomas.watkins@zego.com>
2022-08-19 11:29:32 +0100

    cleanup test model

--- app/models/models.py
+++ app/models/models.py
@@ -4,11 +4,3 @@ from tortoise import models, fields
 class TestModel(models.Model):
     id = fields.IntField(pk=True)
     name = fields.TextField()
-
-class TestModelTwo(models.Model):
-    id = fields.IntField(pk=True)
-    name = fields.TextField()
-
-class TestModelThree(models.Model):
-    id = fields.IntField(pk=True)
-    name = fields.TextField()
