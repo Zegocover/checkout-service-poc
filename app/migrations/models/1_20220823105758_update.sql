4d00f33cff489a801ed6f67cff7353bcd576e7c7
twatkins92 <thomas.watkins@zego.com>
2022-08-23 12:12:44 +0100

    add model migration

---
+++ app/migrations/models/1_20220823105758_update.sql
@@ -0,0 +1,20 @@
+-- upgrade --
+CREATE TABLE IF NOT EXISTS "checkoutitemdb" (
+    "id" UUID NOT NULL  PRIMARY KEY,
+    "type" TEXT NOT NULL,
+    "amount" DECIMAL(30,2) NOT NULL,
+    "description" TEXT NOT NULL,
+    "external_id" UUID NOT NULL,
+    "checkout_session_id_id" UUID NOT NULL REFERENCES "checkoutsessiondb" ("id") ON DELETE CASCADE
+);;
+CREATE TABLE IF NOT EXISTS "checkoutsessiondb" (
+    "id" UUID NOT NULL  PRIMARY KEY,
+    "session_token" UUID NOT NULL UNIQUE,
+    "success_url" VARCHAR(2048) NOT NULL,
+    "cancel_url" VARCHAR(2048) NOT NULL
+);;
+DROP TABLE IF EXISTS "testmodel";
+DROP TABLE IF EXISTS "testmodeltwo";
+-- downgrade --
+DROP TABLE IF EXISTS "checkoutitemdb";
+DROP TABLE IF EXISTS "checkoutsessiondb";
