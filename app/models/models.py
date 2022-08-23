7b6c5c4c6254bac7f4644528558a510d929fcdea
twatkins92 <thomas.watkins@zego.com>
2022-08-23 10:47:05 +0100

    add models

--- app/models/models.py
+++ app/models/models.py
@@ -9,3 +9,27 @@ class TestModel(models.Model):
 class TestModel2(models.Model):
     id = fields.IntField(pk=True)
     name = fields.TextField()
+
+
+
+class CheckoutSessionDb(models.Model):
+    id = fields.UUIDField(pk=True)
+    session_token = fields.UUIDField(unique=True)
+    success_url = fields.TextField()
+    cancel_url = fields.TextField()
+
+    def __str__(self):
+        return str(self.session_token)
+
+class CheckoutItemDb(models.Model):
+    id = fields.UUIDField(pk=True)
+    type = fields.TextField()
+    amount = fields.DecimalField()
+    description = fields.TextField()
+    checkout_session_id = fields.ForeignKeyField(model_name="CheckoutSessionDb", related_name="items")
+    external_id = fields.UUIDField()
+
+    def __str__(self):
+        return f"{self.description} {self.amount}"
+
+
