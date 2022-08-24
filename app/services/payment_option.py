2bf9741ee236e954db846dc815a1d84eb8a5b356
craig.richardson <craigargh@gmail.com>
2022-08-24 10:03:49 +0100

    Refactor payment options

--- app/services/payment_option.py
+++ app/services/payment_option.py
@@ -1,22 +1,18 @@
-from tabnanny import check
-from tortoise import models, fields
-from uuid import UUID, uuid4
+from dataclasses import dataclass
 
 from services.session_models import CheckoutSession
 
 
+@dataclass
 class StripePaymentOption:
-    def __init__(self) -> None:
-        pass
+    name: str = "Stripe"
 
-    @classmethod
     async def is_available(self, checkout_session) -> bool:
         """
         Works out whether payment option is available for that particular checkout session
         """
         return True
 
-    @classmethod
     def get_description(self, checkout_session) -> str:
         """
         Used when the description needs to be dynamic, e.g. explaining monthly payments for a credit agreement
@@ -24,7 +20,6 @@ class StripePaymentOption:
         """
         return "Stripe Payment Description"
 
-    @classmethod
     def payment_session_setup(self, checkout_session) -> str:
         """
         Does any setup required before redirecting to the payment provider
@@ -32,7 +27,6 @@ class StripePaymentOption:
         """
         pass
 
-    @classmethod
     def payment_session_redirect_url(self, checkout_session) -> str:
         """
         Builds the url that the user will be redirect to in order to make a payment
@@ -40,11 +34,10 @@ class StripePaymentOption:
         return "https://stripe.com/docs/"
 
 
+@dataclass
 class PCLPaymentOption:
-    def __init__(self) -> None:
-        pass
+    name: str = "PCL"
 
-    @classmethod
     async def is_available(self, checkout_session) -> bool:
         items = checkout_session.checkout_items
         if [i for i in items if i.type == "Quote"]:
@@ -52,8 +45,6 @@ class PCLPaymentOption:
         else:
             return False
 
-   
-    @classmethod
     def get_description(self, checkout_session) -> str:
         """
         Used when the description needs to be dynamic, e.g. explaining monthly payments for a credit agreement
@@ -61,7 +52,6 @@ class PCLPaymentOption:
         """
         return "PCL Payment Description"
 
-    @classmethod
     def payment_session_setup(self, checkout_session) -> str:
         """
         Does any setup required before redirecting to the payment provider
@@ -69,7 +59,6 @@ class PCLPaymentOption:
         """
         pass
 
-    @classmethod
     def payment_session_redirect_url(self, checkout_session) -> str:
         """
         Builds the url that the user will be redirect to in order to make a payment
@@ -78,22 +67,28 @@ class PCLPaymentOption:
 
 
 async def get_payment_options(checkout_session: CheckoutSession):
-    available_payment_options = []
     return_list = []
-    option_dict = dict()
 
-    for payment_option in [StripePaymentOption, PCLPaymentOption]:
-        if await payment_option.is_available(checkout_session):
-            available_payment_options.append(payment_option)
+    all_options = [
+        StripePaymentOption(),
+        PCLPaymentOption(),
+    ]
 
-    
+    available_payment_options = [
+        payment_option
+        for payment_option in all_options
+        if await payment_option.is_available(checkout_session)
+    ]
+
     for option in available_payment_options:
-        option_dict["payment_method"] = option.__name__
-        option_dict["description"] = payment_option.get_description(checkout_session)
-        option_dict["redirect_url"] = payment_option.payment_session_redirect_url(checkout_session)
-        option_dict["success_url"] = checkout_session.success_url
-        option_dict["cancel_url"] = checkout_session.cancel_url
-        option_dict["amount"] = checkout_session.total()
+        option_dict = {
+            "payment_method": option.name,
+            "description": option.get_description(checkout_session),
+            "redirect_url": option.payment_session_redirect_url(checkout_session),
+            "success_url": checkout_session.success_url,
+            "cancel_url": checkout_session.cancel_url,
+            "amount": checkout_session.total(),
+        }
 
         return_list.append(option_dict)
 
